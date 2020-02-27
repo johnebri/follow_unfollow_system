71562debe0e7b43358975973571131d3b6689eb5
johnebri <john.ebr@yahoo.com>
2020-02-27 19:52:51 +0100

    login page

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/css/custom.css" rel="stylesheet">
-    <link href="assets/font/css/all.css" rel="stylesheet">
-    <link href="assets/css/jquery.scrollbar.css" rel="stylesheet">
+    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/custom.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/font/css/all.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/jquery.scrollbar.css')}}" rel="stylesheet">
     @yield('styles')
 </head>
 
 <body>
     @yield('content')
-    <script src="assets/js/jquery-latest.min.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/jquery.scrollbar.js"></script>
+    <script src="{{asset('assets/js/jquery-latest.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.scrollbar.js')}}"></script>
     <script>
         $(document).ready(function() {
             $('.scrollbar-inner').scrollbar();
